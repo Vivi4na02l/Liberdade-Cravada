28cf4a20889fec4adfe9da502cf5eb6590be3669
Viviana <40200339@esmad.ipp.pt>
2025-04-03 23:49:23 +0100

    created html page where the games are going to be played.

--- javascript/jogosEducativos.js
+++ javascript/jogosEducativos.js
@@ -1,7 +1,17 @@
-function setup() {
-    let barW = document.querySelector('#timelineBarP5').offsetWidth;
-    let barH = document.querySelector('#timelineBarP5').offsetHeight;
+let barW, barH, ball, ballMove;
 
+function setup() {
+    barW = document.querySelector('#timelineBarP5').offsetWidth;
+    barH = document.querySelector('#timelineBarP5').offsetHeight;
+
+    ball = {
+        x: barW*0.1,
+        y: barH/2,
+        size: barH
+    }
+
+    ballMove = false;
+    
     console.log(barW +' '+ barH);
 
     let timelineCanvas = createCanvas(barW, barH);
@@ -9,13 +19,10 @@ function setup() {
 }
   
 function draw() {
-    let barW = document.querySelector('#timelineBarP5').offsetWidth;
-    let barH = document.querySelector('#timelineBarP5').offsetHeight;
-
     let beige = "#FDEBD0";
     let red = "#C0392B";
 
-    // clear();
+    clear();
     noStroke();
 
     //* entire rectangle of timeline bar */
@@ -24,15 +31,34 @@ function draw() {
 
     //* red ball */
     fill(red);
-    circle(barW*0.1, barH/2, barH); /* (x, y, diameter) */
+    circle(ball.x, ball.y, ball.size); /* (x, y, diameter) */
 
     //* red part of the timeline bar that precedes the red ball */
     fill(red);
-    // rect(0, barH/2-barH/4, );
+    rect(0, barH/2-barH/4, ball.x, barH/2);
 }
 
 function mousePressed() {
-    fill(red);
-    circle(mouseX, barH/2, barH);
+    let distance = mouseX - ball.x
+
+    console.log(mouseX +' '+ ball.x+' '+ball.size+' '+distance);
+    
+
+    if (distance < ball.size/2) {
+        ballMove = true;
+        console.log('yes');
+        
+    } else {
+        ballMove = false;
+    }
 }
-  
+
+function mouseDragged() {
+    if (ballMove) {
+        ball.x = mouseX;
+    }
+}
+
+function mouseRelease() {
+    ballMove = false;
+}

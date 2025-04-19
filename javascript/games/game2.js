2602fae47cbd2a22c44716c08277b34d137fea8a
Viviana <40200339@esmad.ipp.pt>
2025-04-19 19:03:24 +0100

    added scenery moving and floor refreshing

--- javascript/games/game2.js
+++ javascript/games/game2.js
@@ -1,8 +1,10 @@
 /** game rules and scenery variables */
 let gameSpeed = 1, ms;
+let scenery = "subway";
 let backgroundW, backgroundH, backgroundX, backgroundY;
 
 let floor_subway, floor_subway_bg, bg_subway;
+let floors = [];
 
 /** character variables */
 let charX, charY, charW, charH, charYDefault;
@@ -41,7 +43,7 @@ export function game2_setup() {
     //* floor
     floorH = height*0.4;
     floorW = (floor_subway.width*floorH)/floor_subway.height /* makes width of image proportionate to its height. Assuming the img "floor_subway" dimensions are gonna be standard for other floor images */
-    floorX = floorW/2;
+    floorX = width-floorW/2;
     floorY = height - floorH/2;
     floorYPlacement = floorY - floorH/4;
 
@@ -73,21 +75,46 @@ export function game2_draw() {
 
     //************ MOVEMENT and SCENERY */
     gameSpeed += 0.0001;
-    obsX -= 5 * gameSpeed;
+    backgroundX -= 0.25;
 
     /** BACKGROUND */
     image(bg_subway, /* img */
         backgroundX, backgroundY*0.8, /* x, y */
         backgroundW, backgroundH) /* w, h */
-    
+        
+        
     //************ FLOOR */
-    image(floor_subway, /* img */
-        floorX, floorY, /* x, y */
-        floorW, floorH) /* w, h */
+    if (floors.length == 0) {   
+        /* Runs the looping depending on how many floors are needed in the screen at the same time to cover its width completely */ /** Math.ceil -> 1.00001 = 2 */
+        for (let i = 0; i < (Math.ceil(width / floorW)+1) ; i++) {
+            if (floors.length == 0) {
+                floors.push(new Floor((width-floorW/2 - floorW), scenery)); /** width-floorW/2 - floorW */ /* the scenery moves from right to left, so naturally the first image needs to be added on the extreme left, this bit of code makes it so the amount left empty on the left screen is filled with the first floor image */
+            } else {
+                floors.push(new Floor((floors[i-1].floorX + floorW), scenery));
+            }
+        }
+    }
 
-    image(floor_subway_bg, /* img */
-        floorX, floorY/2+floorH/2-((floor_subway_bg.height*floorW)/floor_subway_bg.width)/2, /* x, y */
-        floorW, (floor_subway_bg.height*floorW)/floor_subway_bg.width) /* w, h */
+    console.log(floors);
+    
+    for (let i = floors.length - 1; i >= 0; i--) {
+        floors[i].update();
+        floors[i].display();
+
+        /** if floor is offscreen, delete it and add a new one */
+        if (floors[i].offscreen()) {
+            floors.splice(i, 1);
+            floors.push(new Floor((floors[floors.length-1].floorX + floorW), scenery));
+        }
+    }
+
+    // image(floor_subway, /* img */
+    //     floorX, floorY, /* x, y */
+    //     floorW, floorH) /* w, h */
+        
+    // image(floor_subway_bg, /* img */
+    //     floorX, floorY/2+floorH/2-((floor_subway_bg.height*floorW)/floor_subway_bg.width)/2, /* x, y */
+    //     floorW, (floor_subway_bg.height*floorW)/floor_subway_bg.width) /* w, h */
 
 
     //************ CHARACTER */
@@ -133,7 +160,7 @@ export function game2_draw() {
     //************ OBSTACLES */
     /** spawns the first obstacle */
     if (frameCount == 10) {
-        obstacles.push(new Obstacle(gameSpeed, width, obsY, obsW, obsH)); //createVector(width/2, height-(imgPencil.width*0.2)), imgPencilRotate, imgPencil.height*0.2
+        obstacles.push(new Obstacle(gameSpeed, width, obsY, obsW, obsH));
     }
 
     for (let i = obstacles.length - 1; i >= 0; i--) {
@@ -143,7 +170,7 @@ export function game2_draw() {
         /** if obstacle is offscreen, delete it and add a new one */
         if (obstacles[i].offscreen()) {
             obstacles.splice(i, 1);
-            obstacles.push(new Obstacle(gameSpeed, width, obsY, obsW, obsH)); //createVector(width/2, height-(imgPencil.width*0.2)), imgPencilRotate, imgPencil.height*0.2
+            obstacles.push(new Obstacle(gameSpeed, width, obsY, obsW, obsH));
         }
     }
 }
@@ -195,8 +222,44 @@ function charIsJumping(heightOfJump, hoveringEnd) {
     }
 }
 
+class Floor {
+    constructor(floorX, whichScenery) { //gameSpeed, width, floorY, floorW, floorH, i, scenery
+        this.floorX = floorX; //width + (floorW/2) totalmente gone -------- width - (floorW/2) aparece a imagem toda encostada à direita
+        this.whichScenery = whichScenery;
+    }
+    
+    update() {
+        // if (previousFloorX == "none") {
+        //     this.floorX = width - (floorW/2)
+        // }
+
+        this.floorX -= 5 * gameSpeed;
+        /** "minus" to make the floor move from right to left
+         * "5" is the default speed
+         * "speed" is used to make it go left faster each frame as the original "gameSpeed" variable increases
+         */
+    }
+    
+    display() { /** makes the obstacle appear on the canvas */
+        if (this.whichScenery == "subway") {
+            image(floor_subway, /* img */
+                this.floorX, floorY, /* x, y */
+                floorW, floorH) /* w, h */
+
+            image(floor_subway_bg, /* img */
+                this.floorX, floorY/2+floorH/2-((floor_subway_bg.height*floorW)/floor_subway_bg.width)/2, /* x, y */
+                floorW, (floor_subway_bg.height*floorW)/floor_subway_bg.width) /* w, h */
+        }
+    }
+    
+    offscreen() {
+        /** if obstacle left the screen */
+        return (this.floorX + (floorW/2) < 0);
+    }
+}
+
 class Obstacle {
-    constructor(speed, width, obsY, obsW, obsH) { // pos, angle, hitbox
+    constructor(speed, width, obsY, obsW, obsH) {
         this.speed = speed;
         this.obsY = obsY;
         this.obsW = obsW;

e0717fac70678b2be8c07d02250f897e32f0bc92
Viviana <40200339@esmad.ipp.pt>
2025-05-03 14:53:22 +0100

    Bettered the UI in html JogosEducativos

--- javascript/jogosEducativos.js
+++ javascript/jogosEducativos.js
@@ -1,5 +1,8 @@
 //* Detects which game was chosen and redirects user to the page with such game
 for (const gameClicked of document.querySelectorAll(".divGame")) {
+    // let gameTitle = gameClicked.id
+    // gameClicked.children[0].innerHTML = gameTitle /** dá o nome  */
+
     gameClicked.addEventListener("click", () => {
         let game = gameClicked.id;
         sessionStorage.setItem("game", game);

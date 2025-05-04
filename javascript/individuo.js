cc70c775765d196130d0e5d4e489a63789215680
Viviana <40200339@esmad.ipp.pt>
2025-05-04 23:38:15 +0100

    added info about marcello and humberto and added interliked connections between characters

--- javascript/individuo.js
+++ javascript/individuo.js
@@ -17,8 +17,8 @@ let characters = [
         char: "woman",
         img: "../../images/website/images/char_woman.png",
         title: "Mulher",
-        info: ['"Cuidadora e educadora, subordinada ao marido";',"O regime promovia a ideia de que o lugar da mulher era em casa;","Apenas mulheres chefes de família (sem marido presente) podiam votar."],
-        text: ['Dia 16 de março de 1933, num discurso, Salazar disse que "o trabalho da mulher fora do lar desagrega este, separa os membros da família, torna-os um pouco estranhos uns aos outros. Desaparece a vida comum, sofre a obra educativa das crianças, diminui o número delas. E com o mau ou impossível funcionamento da alimentação e do vestuário verifica-se uma perda importante, raro, materialmente compensado pelo salário recebido". Salazar acreditava que a <i>missão natural</i> de uma mulher era procriar e apresentar submissão à sua cara metade.',
+        info: ['Descrita como "Cuidadora e educadora, subordinada ao marido";',"O regime promovia a ideia de que o lugar da mulher era em casa;","Apenas mulheres chefes de família (sem marido presente) podiam votar."],
+        text: ['Dia 16 de março de 1933, num discurso, <a id="salazar" class="redirect red">Salazar</a> disse que "o trabalho da mulher fora do lar desagrega este, separa os membros da família, torna-os um pouco estranhos uns aos outros. Desaparece a vida comum, sofre a obra educativa das crianças, diminui o número delas. E com o mau ou impossível funcionamento da alimentação e do vestuário verifica-se uma perda importante, raro, materialmente compensado pelo salário recebido". <a id="salazar" class="redirect red">Salazar</a> acreditava que a <i>missão natural</i> de uma mulher era procriar e apresentar submissão à sua cara metade.',
             "Apesar de a Constituição de 1933 proclamar igualdade, na práticas as mulheres enfrentavam muitas restrições: <li id='charInfo' class='m0 IBM fontNormalSize'>Necessitavam do marido para trabalhar, viajar ou abrir contas bancárias;</li><li id='charInfo' class='m0 IBM fontNormalSize'>Eram excluídas de profissões como juíza, diplomata ou militar;</li><li id='charInfo' class='m0 IBM fontNormalSize'>Ganhavam menos que os homens pelo mesmo trabalho;</li><li id='charInfo' class='m0 IBM fontNormalSize'>O Código Cívil de 1867, em vigor até 1967, estabelecia que a mulher devia obediência ao marido e não podia gerir os seus bens sem consentimento.</li>",
             "Para moldar as mulheres segundos os ideais conservadores do Estado, em 1937, foi criada a Mocidade Portuguesa Feminina (MPF), uma organização obrigatória para jovens raparigas, que ensinava a importância do lar, da moral e da obediência.",
             "O direito de voto era concedido a mulheres chefes de família, ou solteiras independentes com curso secundário ou superior (a educação naquela altura era cara, e, para mulheres, desencorajada).<br><br>A prostituição era considerada, em certas circunstâncias, um crime, tal como a questão ao aborto."
@@ -35,8 +35,12 @@ let characters = [
         char: "humberto",
         img: "../../images/website/images/char_humberto_delgado.png",
         title: "Humberto Delgado",
-        info: ["1","2","3"],
-        text: ["oaosdkoekqwdoas"]
+        info: ["Nasceu dia 15 de maio de 1906;",'Conhecido como "General sem medo";','"Quiseram calar-nos... Não se assassina a liberdade";',"Morreu dia 13 de fevereiro de 1965."],
+        text: ['Humberto Delgado, conhecido como o "General sem medo", foi uma figura central na oposição ao regime ditatorial de Salazar. Iniciou a sua carreira militar no Colégio Militar e destacou-se na Aeronáutic, sendo um dos fundadores da TAP em 1945. A sua carreira militar incluiu missões internacionais e cargos de destaque, mas foi afastado por razões políticas.',
+            'Em 1958, apresentou-se como candidato independente às eleições presidenciais, desafiando abertamente Salazar com a célebre frase "Obviamente, demito-o". A sua campanha mobilizou uma onda de apoio popular, especialmente no norte do país, mas foi marcada por repressão policial e manipulação eleitoral. Apesar das adversidades, obteve cerca de 25% dos votos, segundos os resultados oficiais, embora se suspeite de fraude eleitorial.',
+            "Após as eleições, foi expulso das forças armadas e exilou-se no Brasil, continuando a luta contra o regime. Em 1964, fundou a Frente Patriótica de Libertação Nacional em Roma, defendendo a necessidade de um golpe militar para derrubar o Estado Novo. Em fevereiro de 1965, foi atraído por um emboscada pela polícia política, PIDE, perto de Badajoz, Espanha, onde foi assassinado.",
+            "O assassinato dee Humberto Delgado foi um dos crimes mais marcantes do regime de <a id='salazar' class='redirect red'>Salazar</a>, evidenciando a brutalidade de repressão política. A sua morte provocou indignação internacional e reforçou a oposição ao regime, contribuindo para o seu isolamento. Hoje, Humberto Delgado é lembrado como um símbolo de resistência e da luta pela democracia em Portugal."
+        ]
     },
     {
         char: "military",
@@ -63,8 +67,12 @@ let characters = [
         char: "marcello",
         img: "../../images/website/images/char_marcello.png",
         title: "Marcello Caetano",
-        info: ["1","2","3"],
-        text: ["oaosdkoekqwdoas"]
+        info: ["Nasceu dia 17 de agosto de 1906;","Foi chefe do governo português entre 1968 e 1974;","Morreu dia 26 de outubro de 1980."],
+        text: ["Após licenciar-se em Direito e doutorar-se em Ciências Político-Económicas, tornou-se professor catedrático na Faculdade de Direito da Universidade de Lisboa. Iniciou a sua carreira política nos anos 1930, alinhando-se com o regime do Estado Novo. Em 1968, após o afastamento de <a id='salazar' class='redirect red'>Salazar</a> por motivos de saúde, foi nomeado Presidente do Conselho de Ministros, tornando-se o último líder do regime autoritário português.",
+            "Durante o seu governo, Caetano tentou implementar reformas moderadas, como a substituição da <a id='PIDE' class='redirect red'>PIDE</a> pela <a id='PIDE' class='redirect red'>DGS</a> e a suavização da censura. Numa tentativa de acalmar as diversas fações, Caetano prometeu aos conservadores continuidade do Estado Novo até então estabelecido e aos liberais deu esperança de renovação. No entanto, contrariamente ao desejado, o regime continuou a enfrentar oposição crescente.",
+            "A Guerra Colonial e a insatisfação popular culminaram na Revolução dos Cravos, a 25 de abril de 1974, que pôs fim ao Estado Novo. Caetano foi deposto e exilado para o Brasil, onde retomou a sua atividade académica.",
+            'Marcello Caetano faleceu a 26 de outubro de 1980, aos 74 anos, vítima de ataque cardíaco, no Rio de Janeiro. Durante o exílio, publicou obras como "Minhas Memórias de Salazar" e "A verdade sobre o 25 de Abril", oferecendo a sua perspetiva sobre o regime e a sua queda.'
+        ]
     },
     {
         char: "salazar",
@@ -73,11 +81,21 @@ let characters = [
         info: ["Nasceu dia 28 de abril de 1889;", "Tornou-se a personagem política mais marcante do século XX português;","Foi chefe do governo português entre 1932 e 1968;","Morreu dia 27 de julho de 1970."],
         text: ["Salazar era uma pessoa com ideais conservadores, católicos e autoritários. Estudou e conclui o curso de Direito na Universidade de Coimbra, onde mais tarde se tornou professor de Economia Política e Finanças. Em 1921, foi eleito deputado pelo Centro Católico Português (CCP), partido que ajudou a fundar. No entanto, renunciou ao cargo poucos dias depois.",
             'A sua participação no CCP e os seus conhecimentos económicos levaram-no a ser chamado pelo governo em 1926 para o cargo de Ministro das Finanças. No entanto, renunciou após poucos dias por não lhe serem concedidos os poderes necessários para implementar as reformas que considerava essenciais. Em 1928, foi novamente convidado para o mesmo cargo, desta vez com plenos poderes sobre as finanças públicas. Conseguiu equilibrar as contas do Estado, sendo apelidado pelo público como "Salvador da Pátria".',
-            "Com o prestígio adquirido, em 1932, foi nomeado Presidente do Conselho de Ministros (equivalente a Primeiro-Ministro). Em 1933, instituiu uma nova Constituição que estabeleceu o Estado Novo, um regime autoritário que defendi valores como Deus, Pátria e Autoridade. Implementou leis opressivas e controladoras, acreditando que a liberdade de expresão poderia ser perigosa e uma menor valia para o país. Estabeleceu a censura, criou a polícia política com autoridade para prender e torturar qualquer atividade considerada suspeita, e controlou o sistema de ensino.",
+            "Com o prestígio adquirido, em 1932, foi nomeado Presidente do Conselho de Ministros (equivalente a Primeiro-Ministro). Em 1933, instituiu uma nova Constituição que estabeleceu o Estado Novo, um regime autoritário que defendi valores como Deus, Pátria e Autoridade. Implementou leis opressivas e controladoras, acreditando que a liberdade de expresão poderia ser perigosa e uma menor valia para o país. Estabeleceu a censura, criou a <a id='PIDE' class='redirect red'>polícia política</a> com autoridade para prender e torturar qualquer atividade considerada suspeita, e controlou o sistema de ensino.",
             "Governou até 1968, quando uma queda que lhe provocou derrame cerebral o afastou do poder. Faleceu dois nos depois, em 1970."
         ]
     },
 ];
+
+function redirectChar() {
+    for (const redirect of document.querySelectorAll(".redirect")) {
+        redirect.addEventListener('click', () => {
+            sessionStorage.setItem("char", redirect.id);
+
+            fillRightInfo(false, 0)
+        });
+    }
+}
 
 document.querySelector("#charGoBack").addEventListener("click", () => {
     window.location.href = "./MateriaisEducativos.html";
@@ -91,7 +109,7 @@ document.querySelector("#page2").addEventListener("click", () => {
     fillRightInfo(true, 2);
 });
 
-fillRightInfo(false, 0)
+fillRightInfo(false, 0);
 
 function fillRightInfo(clicked, page) {
     let char = sessionStorage.getItem("char");
@@ -109,6 +127,7 @@ function fillRightInfo(clicked, page) {
 
     fillInfo(chosenChar);
     fillText(chosenChar, clicked, page);
+    redirectChar();
 }
 
 function fillInfo(chosenChar) {
@@ -143,6 +162,7 @@ function fillText(chosenChar, pageClicked, page) {
             }
         }
         document.querySelector('#pages').style.display = "none";
+        document.querySelector('#charAbout').classList.remove("jcsb");
         document.querySelector('#charText').innerHTML = info;
     } else {
         if (pageClicked) {

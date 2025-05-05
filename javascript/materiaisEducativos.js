36725e9d6ed8c1f16fd7489ad4d05ac5d39dbac4
Viviana <40200339@esmad.ipp.pt>
2025-05-05 06:35:19 +0100

    all informational content added

--- javascript/materiaisEducativos.js
+++ javascript/materiaisEducativos.js
@@ -17,43 +17,34 @@ let content = [
         allText: [ /** posPage */
             { page: 1,
                 active: true,
-                text: "texto1" },
+                text: "Em 1930, Salazar queria mudar completamente a forma como Portugal era governado. Enquanto os partidos que defendiam a democracia tentavam, sem sucesso, revoltas para voltar ao sistema anterior, Salazar trabalhava para criar novas regras e instituições que dessem estabilidade ao país. Não querendo que o país voltasse ao sistema político da Primeira República, que permitia muitos partidos e debates no parlamento, criou a <span class='red underlined'>União Nacional, o único partido permitido</span>, que reunia todos os que apoiavam o regime e queriam servir o país sob a sua liderança.<br><br> Adicionalmente, também em 1930, foi aprovado o Ato Colonial, uma lei que definia como Portugal devia controlar as suas colónias (territórios em África e na Ásia), centralizando o poder em Lisboa e <span class='red underlined'>retirando autonomia às colónias</span>." },
             { page: 2,
                 active: false,
-                text: "texto2" },
+                text: "Em 1933, entrou em vigor uma nova Constituição que estabeleceu o <span class='red underlined'>Estado Novo</span>, um regime autoritário liderado por Salazar, também conhecido como <span class='red underlined'>Salazarismo</span>. Salazar tornou-se o líder principal do País, com poderes quase absolutos. Ainda em 1933, foi criado o Estatuto do Trabalho Nacional, que organizava as relações laborais de forma a evitar greves, que era, até então, um direito do povo, e conflitos entre patrões e trabalhadores.<br><br> De forma a proteger o regime de Salazar, foi fundada a <span class='red underlined'>Polícia de Vigilância e Defesa do Estado (PVDE)</span>, que tinha duas funções: <li>Vigiava e reprimia quem fosse contra o governo, como opositores políticos e sindicalistas;</li> <li>Controlava a entrada e saída de pessoas do país, especialmente estrangeiros, para evitar ameaças externas.</li>" },
             { page: 3,
                 active: false,
-                text: "texto3" },
+                text: 'A <span class="red underlined">censura</span> foi uma ferramenta usada pelo regime para controlar a informação e manter a ordem. Começou em 1926 e foi intensificada com o Estado Novo, também em 1933. A censura afetava: <br><br><li>Imprensa: Jornais e revistas eram revistos antes de serem publicados;</li><li>Literatura: Livros considerados perigosos eram proibidos ou retirados do mercado;</li><li>Teatro e Cinema: Peças e filmes eram censurados para evitar críticas ao regime;</li><li>Rádio e Televisão: Programas eram controlados para garantir que não houvesse conteúdo contrário ao governo.</li><br>A censura levava autores e jornalistas a praticarem a autocensura, evitando temas que pudessem ser problemáticos. Isso limitava a liberdade de expressão e o desenvolvimento cultural.' },
+            { page: 4,
+                active: false,
+                text: 'A 18 de janeiro de 1934, trabalhadores organizaram uma greve geral contra as políticas do regime. A PVDE respondeu com repressão severa: muitos foram presos, e alguns enviados para prisões políticas ou campos de concentração, como o do Tarrafal, em Cabo Verde. <br><br> De forma a intensificar as defesas ao regime, foi criada a Legião Portuguesa, uma organização militar armada.' },
         ]
     },
     {
         year: 1945,
-        title: "Fim da II Guerra Mundial",
+        title: "Criação da PIDE",
         allText: [ /** posPage */
             { page: 1,
                 active: true,
-                text: "texto1" },
-            { page: 2,
-                active: false,
-                text: "texto2" },
-            { page: 3,
-                active: false,
-                text: "texto3" },
+                text: "A PVDE foi transformada na <span class='red underlined'>Polícia Internacional de Defesa do Estado (PIDE)</span>, no contexto do fim da Segunda Guerra Mundial, com o intuito de transmitir uma imagem menos opressora. Apesar da mudança de nome, as funções continuaram semelhantes, mas com mais poder. A PIDE era conhecida por prender e torturar opositores do regime.<br><br>Em 1953, foi implementado o primeiro Plano de Fomento, focado na construção de infraestruturas como barragens, estradas e escolas técnicas, visando modernizar a economia portuguesa. Apesar desses esforços, o país continuava economicamente atrasado em relação a outros países europeus." },
         ]
     },
     {
         year: 1958,
-        title: "Humberto Delgado Exilado",
+        title: "Humberto Delgado",
         allText: [ /** posPage */
             { page: 1,
                 active: true,
-                text: "texto1" },
-            { page: 2,
-                active: false,
-                text: "texto2" },
-            { page: 3,
-                active: false,
-                text: "texto3" },
+                text: "As eleições presidenciais de 1958 representaram um momento crítico para o regime. O general <span class='red underlined'>Humberto Delgado</span> apresentou-se como candidato da oposição, mobilizando grande apoio popular. Apesar disso, Américo Tomás, um oficial da Marinha Portuguesa, foi declarado vencedor em eleições marcadas por alegações de fraude. Como resultado, em 1959, a Constituição foi alterada para que o Presidente da República passasse a ser eleito por um colégio eleitoral restrito, eliminando a possibilidade de candidaturas independentes.<br><br> <i>Embora o Presidente fosse, em teoria, o chefe de Estado, no contexto do Estado Novo, o poder real estava concentrado nas mãos de António de Oliveira Salazar, o Presidente do Conselho.</i>" },
         ]
     },
     {
@@ -62,13 +53,13 @@ let content = [
         allText: [ /** posPage */
             { page: 1,
                 active: true,
-                text: "texto1" },
+                text: "Entre 1961 e 1968, <span class='red underlined'>Portugal esteve envolvido em três guerras em África</span>, tentando manter o controlo sobre as colónias de Angola, Guiné-Bissau e Moçambique. Estas guerras começaram quando os povos locais, cansados da exploração e da falta de liberdade, decidiram lutar pela independência. <br><br><li><span class='red underlined'>Angola (1961):</span> A guerra começou com protestos de camponeses na Baixa do Cassange, que foram violentamente reprimidos pelas autoridades portuguesas, resultando em centenas de mortos. Pouco depois, em Luanda, ocorreram ataques a prisões e esquadras de polícia. Em março, a União dos Povos de Angola (UPA) lançou ataques no norte do país, matando muitos colonos e africanos aliados dos portugueses;</li>" },
             { page: 2,
                 active: false,
-                text: "texto2" },
+                text: "<li><span class='red underlined'>Guiné-Bissau (1963):</span> A luta armada foi iniciada, pelo Partido Africano para a Independência da Guiné e Cabo Verde (PAIGC) após anos de tentativas falhadas de diálogo com Portugal;</li> <br> <li><span class='red underlined'>Moçambique (1964):</span> Após o massacre de Mueda em 1960, onde dezenas de manifestantes foram mortos, a Frente de Libertação de Moçambique (FRELIMO) iniciou a luta armada em 1964. A guerrilha espalhou-se rapidamente, especialmente nas regiões norte e centro do país.</li>" },
             { page: 3,
                 active: false,
-                text: "texto3" },
+                text: "Entre 1961 e 1974, mais de um milhão de portugueses foram mobilizados para a guerra. Muitos sofreram traumas psicológicos que duraram toda a vida. A guerra também teve um impacto profundo na sociedade portuguesa, com muitas famílias a perderem entes queridos e a viverem com o medo constante.<br><br> Em 1965, o general <span class='red underlined'>Humberto Delgado foi assassinado pela PIDE</span>, numa emboscada na fronteira com Espanha. Este crime gerou uma onda de revolta nacional e internacional, intensificando o movimento de oposição." },
         ]
     },
     {
@@ -77,13 +68,13 @@ let content = [
         allText: [ /** posPage */
             { page: 1,
                 active: true,
-                text: "texto1" },
+                text: "Em 1968, o ditador <span class='red underlined'>Salazar</span> sofreu uma queda grave da cadeira, batendo com a cabeça, o que o deixou incapacitado, <span class='red underlined'>acabando por ser afastado do poder</span>.<br><br>No seu lugar, entrou <span class='red underlined'>Marcello Caetano</span>, que tentou manter o regime, mas ao mesmo tempo introduzir algumas reformas moderadas. Tentou agradar aos conservadores que defendiam o regime autoritário e também aos liberais que pediam mais liberdade. No entanto, não conseguiu satisfazer nenhum dos lados: os conservadores viam-no como fraco e os liberais como demasiado lento e limitado. A PIDE mudou de nome para DGS (Direção-Geral de Segurança), numa tentativa de suavizar a imagem do regime, mas a repressão continuou.<br><br>Dois anos depois, dia 27 de julho de 1970, <span class='red underlined'>António de Oliveira de Salazar morreu</span>." },
             { page: 2,
                 active: false,
-                text: "texto2" },
+                text: "Entretanto as guerras coloniais em África, em Angola, Moçambique e Guiné-Bissau, continuavam. Os militares portugueses estavam cada vez mais descontentes com o conflito prolongado. <br><br> Em 1968, começaram a surgir protestos em Portugal contra a guerra, especialmente entre os estudantes. Muitos jovens recusaram-se a combater, fugindo do país ou desertando. A pressão social e os custos da guerra contribuíram para o crescente descontentamento entre os militares." },
             { page: 3,
                 active: false,
-                text: "texto3" },
+                text: "Em África, os líderes dos movimentos de independência também eram alvos. Eduardo Mondlane, líder da FRELIMO em Moçambique, foi assassinado em 1969. Em 1973, foi Amílcar Cabral, líder do PAIGC na Guiné-Bissau e Cabo Verde, que também foi morto. Mesmo com a repressão, a luta pela independência não parava.<br><br> Em 1973, o <span class='red underlined'>Partido Socialista (PS) foi fundado</span> na Alemanha, no exílio, liderado por Mário Soares. <br><br> Também em 1973, a Guiné-Bissau proclamou a sua independência de forma unilateral. Este foi um marco histórico, pois foi a primeira colónia portuguesa a fazê-lo de forma concreta e com reconhecimento internacional. Isso mostrou ao mundo e aos próprios portugueses que o <span class='red underlined'>império estava a desmoronar</span>." },
         ]
     },
     {
@@ -92,13 +83,16 @@ let content = [
         allText: [ /** posPage */
             { page: 1,
                 active: true,
-                text: "texto1" },
+                text: 'Os militares, reunidos no chamado <span class="red underlined">Movimento das Forças Armadas (MFA)</span>, organizaram-se de forma clandestina. Em março, ocorreu uma tentativa falhada de golpe militar. Cerca de 200 militares foram presos. Apesar do fracasso, o movimento não parou. A 24 de março, os líderes do MFA decidiram avançar com um golpe definitivo. <br><br> Na noite de 24 para 25 de abril, tudo foi posto em marcha. A música "E Depois do Adeus", de Paulo de Carvalho, foi o primeiro sinal, seguido da canção "Grândola Vila Morena", de Zeca Afonso, transmitida na Rádio Renascença, que confirmava que o <span class="red underlined">golpe estava em andamento</span>. As tropas começaram então a ocupar os principais pontos estratégicos de Lisboa: o aeroporto, rádios, ministérios, e quartéis.' },
             { page: 2,
                 active: false,
-                text: "texto2" },
+                text: "Milhares de pessoas saíram à rua para apoiar os militares. Os populares ofereceram <span class='red underlined'>cravos aos soldados, que os colocaram nos canos das espingardas</span>, conhecido até hoje como um gesto simbólico que deu nome à <span class='red underlined'>Revolução dos Cravos</span>. <br><br> Ao longo do dia 25 de abril, Marcelo Caetano refugiou-se no Quartel do Carmo, mas, após algumas horas, rendeu-se e o <span class='red underlined'>regime caiu sem grande derramamento de sangue</span>. Similarmente, desde as primeiras horas da revolução, os militares cercavam a sede da DGS, até pouco tempo denominada de PIDE. No entanto, houve repressão final: agentes da polícia política dispararam contra manifestantes, matando quatro pessoas." },
             { page: 3,
                 active: false,
-                text: "texto3" },
+                text: "No dia seguinte, 26 de abril, a Junta de Salvação Nacional assumiu o poder. Os presos políticos foram libertados, os antigos líderes do regime foram enviados para o exílio e Spínola, general do Exército português, tornou-se Presidente da República. Portugal começou uma nova era. <br><br> Nos meses seguintes, aconteceram mudanças rápidas. Em maio, foi criado o primeiro Governo Provisório, que, contrariamente ao Estado Novo, era liderado por vários partidos, sendo esses o PS, o PCP e o PSD. O salário mínimo nacional foi fixado pela primeira vez. O regresso de exilados políticos deu força aos partidos. O povo participou em manifestações massivas, como no histórico 1º de Maio, com meio milhão de pessoas em Lisboa, foi um marco de alegria e esperança na luta pela liberdade e direitos dos trabalhadores." },
+            { page: 4,
+                active: false,
+                text: "O país também começou a preparar o <span class='red underlined'>fim do colonialismo</span>. Em maio iniciaram-se conversações com o PAIGC, e em setembro Portugal reconheceu oficialmente a independência da Guiné-Bissau. Em junho, começaram contactos com a FRELIMO, em Moçambique.<br><br> O resto do ano foi marcado por intensa agitação social e política. Foram legalizadas ocupações de casas, criadas leis para proteger o direito à greve, e estabelecido um programa inovador para resolver o problema da habitação. O Governo lançou também campanhas de alfabetização, criou o Serviço Cívico Estudantil e anunciou eleições livres para o ano seguinte.<br><br>Com o passar dos anos, Portugal iniciou um caminho de reconstrução cívica e democrática, onde gradualmente se foram restaurando os direitos fundamentais outrora negados aos cidadãos. Paralelamente, surgiram novas leis e garantias que não apenas protegiam esses direitos, mas também consolidavam os pilares de uma sociedade mais justa, livre e participativa." },
         ]
     },
 ];
